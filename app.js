c917f25b447aa0e43bb8f0f5eb416ff225606ab9
Raquel <calderon.rocky@gmail.com>
2017-07-11 15:00:36 -0700

    adding mysql var to app.js

--- app.js
+++ app.js
@@ -10,6 +10,12 @@ var users = require('./routes/users');
 
 var app = express();
 
+// db setup
+var mysql = require("mysql");
+
+
+
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
